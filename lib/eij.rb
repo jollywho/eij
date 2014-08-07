b3c0ca770c648874c5099c9e5c7ff2e9ebbe4c12
Kevin Vollmer <works.kvollmer@gmail.com>
2014-08-06 22:22:22 -0400

    optargs

--- lib/eij.rb
+++ lib/eij.rb
@@ -5,19 +5,23 @@ module Eij
   key = ARGV[0]
 
   OptionParser.new { |opts|
-    opts.banner = "Usage: #{File.basename($0)} -u -i -s filename"
+    opts.banner = "Usage: #{File.basename($0)} key [-j word] | [-e word] | [-d num [char]]"
 
-    opts.on( '-j', '', 'japanese') do |arg|
+
+    opts.on( '-j', '--japanese WORD', 'japanese') do |v|
+      p v
       #
     end
 
-    opts.on('-e', '', 'english') do |arg|
+    opts.on( '-e', '--english WORD', 'english') do |v|
+      p v
       #
     end
 
-    opts.on('-d', '', 'damage') do |arg|
-      #
+    opts.on( '-d', "--list NUM,char", Array, 'damage') do |v|
+      p v
     end
+
   }.parse!
 
 end
