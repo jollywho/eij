108bfdea25ba61302b0b6a7e28144b0c69273219
Kevin Vollmer <works.kvollmer@gmail.com>
2014-08-07 20:24:40 -0400

    notes and cjk check

--- lib/eij.rb
+++ lib/eij.rb
@@ -1,25 +1,28 @@
 require 'optparse'
+require './eij/string.rb'
 
 module Eij
 
   key = ARGV[0]
 
+  p %x{bash -lic 'source ./func.sh; ej #{key}'}
+  p key.contains_cjk?
+
   OptionParser.new { |opts|
     opts.banner = "Usage: #{File.basename($0)} key [-j word] | [-e word] | [-d num [char]]"
 
-
     opts.on( '-j', '--japanese WORD', 'japanese') do |v|
-      p v
+      puts v.red
       #
     end
 
     opts.on( '-e', '--english WORD', 'english') do |v|
-      p v
+      puts v.bg_green
       #
     end
 
     opts.on( '-d', "--list NUM,char", Array, 'damage') do |v|
-      p v
+      print v[0].blue, v[1].brown
     end
 
   }.parse!
